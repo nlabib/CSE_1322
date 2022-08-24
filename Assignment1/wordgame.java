af167775ea75c7b0a062fe40d94a6ff14437b633
nasimullabib <nlabib@students.kennesaw.edu>
2022-08-24 01:38:27 -0400

    working on Assigment 1

---
+++ Assignment1/wordgame.java
@@ -0,0 +1,20 @@
+public class wordgame{
+    private char[][] gameboard;
+    private boolean[][] revealShow;
+    private int userInputCell;
+    public void fillBoard(){
+
+    }
+    public char printBoard(){
+
+    }
+    public wordgame(int i){
+        if(i%2 == 0){
+            this.userInputCell = i;
+        }
+        else{
+            
+        }
+    }
+
+}
