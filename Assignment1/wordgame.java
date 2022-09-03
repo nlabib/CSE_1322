ca1548a117e9a07954605ef91a16b973e684e043
nasimullabib <nlabib@students.kennesaw.edu>
2022-09-03 16:41:18 -0400

    Almost done

--- Assignment1/wordgame.java
+++ Assignment1/wordgame.java
@@ -1,53 +1,65 @@
 import java.util.Random;
+import java.io.LineNumberInputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class wordgame{
-	private  int userInputCell = 4;
-	private char[][] gameboard = new char[userInputCell][userInputCell];
+	private int userInputCell;
+	private char[][] gameboard;
 	private boolean[][] revealShow;
 	private int index;
 
-//Constructor isn't working
+	//Constructor isn't working
 	//Get the input for the user and check if the number is even.
-/*
-	public void setUserInputCell(){
-		do{
-			System.out.println("How big of a board (Enter an even number)?");
-			Scanner userIn = new Scanner(System.in);
-			this.userInputCell = userIn.nextInt();
-		}while(userInputCell % 2 != 0);
-	}
-	public wordgame(){
-		this.userInputCell = userinput;
-	}
-*/
-	//using random generator to generate the board
-	public  void fillBoard(){
+	Scanner userInput = new Scanner(System.in);
 
-		ArrayList<Character> wordbank = new ArrayList<>();
-		//Generates A to Z in the wordbank arraylist
-		for(char a = 'A'; a <= 'Z'; a++){
-			wordbank.add(a);
+
+
+	public wordgame(int input){
+		if(input%2==0){
+			this.userInputCell = input;
+			revealShow= new boolean[userInputCell][userInputCell];
+			gameboard = new char[userInputCell][userInputCell];
+			fillBoard();
 		}
-		Random random_number = new Random();
-		for(int i = 0; i < gameboard.length; i++){
-			for(int j = 0; j < gameboard.length; j++){
-				// Filling the board
-				index = random_number.nextInt(userInputCell/2);
-				//Check if the location of the array is an open positon
-				if(gameboard[i][j] == '\u0000'){
-					gameboard[i][j] = wordbank.get(index);
-					wordbank.remove(i);
+		else{
+			System.out.println("Dimensions must be even");
+			return;
+		}
+
+
+	}
+
+
+
+	//using random generator to generate the board
+
+	public  void fillBoard(){
+		char myLetter='A';
+		int whenToChange=0;
+		Random myRand= new Random();
+		do{
+			int r=myRand.nextInt(gameboard.length);
+			int c=myRand.nextInt(gameboard.length);
+			if(gameboard[r][c]=='\u0000'){
+				gameboard[r][c]=myLetter;
+				whenToChange++;
+				if(whenToChange%2==0){
+					myLetter++;
 				}
 			}
-		}
+		}while(whenToChange<gameboard.length*gameboard.length);
+
 	}
 
 	public void printBoard(){
+		// Check if its false or not
 		for(int i = 0; i < gameboard.length; i++){
 			for(int j = 0; j < gameboard.length; j++){
 				System.out.print(" | ");
-				System.out.print(gameboard[i][j]);
+				if(revealShow[i][j] == true){
+					System.out.print(gameboard[i][j]);
+				}
+
 			}
 			System.out.print(" | ");
 			System.out.println();
@@ -63,10 +75,21 @@ public class wordgame{
 		this.revealShow[row][colum] = false;
 	}
 	//A method to show position
-	public void allRevealed(){
+	public boolean allRevealed(){
+		for(int i=0; i< gameboard.length; i++){
+			for(int j = 0; j < gameboard[0].length; j++){
+				if(revealShow[i][j] == false){
+					return false;
+				}
+
+			}
+		}
+		return true;
+
 
 	}
 
 
 
 }
+
