0eefc415ec21f948a327f5601f555745af549012
nasimullabib <nlabib@students.kennesaw.edu>
2022-09-04 16:06:41 -0400

    Lab 3 done

--- Lab3/Lab3/src/Driver.java
+++ Lab3/Lab3/src/Driver.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class Driver{
 	public static void main(String[] args){
-		System.out.println("hello");
+		Quiz newquiz = new Quiz();
+		boolean exit = false;
+		int userinput = 0;
+		Scanner userin = new Scanner(System.in);
+		while(userinput != 5){
+			System.out.println("1. Add a question to the quiz");
+			System.out.println("2. Remove a question from the quiz");
+			System.out.println("3. Modify a question in the quiz");
+			System.out.println("4. Take the quiz");
+			System.out.println("5. Quit");
+			System.out.println("What would you like to do?");
+			userinput = userin.nextInt();
+			switch(userinput){
+				case 1:
+				{
+					newquiz.addQuestion();
+					break;
+				}
+				case 2:{
+					newquiz.removeQuestions();
+					break;
+				}
+				case 3:{
+					newquiz.modifyQuestions();
+					break;
+				}
+				case 4: {
+					newquiz.giveQuiz();
+					break;
+				}
+				case 5:{
+					userinput = 5;
+					break;
+				}
+			}
+		}
+
 	}
 }
